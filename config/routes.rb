dbd1907c395dab33d792b868c865403cdaf28ccd
ArthurColle <acolle@yandex.com>
2015-02-04 16:23:45 -0500

    Fixed mis-aligned flash notice messages

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 
   root 'pages#home'
 
-  get 'pages/about'
+  get 'about' => 'pages#about'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
