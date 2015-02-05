de402744f28a07567882eae90058d53805aa5897
ArthurColle <acolle@yandex.com>
2015-02-05 00:28:27 -0500

    Fixing small bugs, including about and contact pages

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   root 'pages#home'
 
   get 'about' => 'pages#about'
+  get 'contact' => 'pages#contact'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
