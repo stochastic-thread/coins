05a17f4ea16b5fb85112613390440719ab84b511
ArthurColle <acolle@yandex.com>
2015-02-04 23:09:16 -0500

    Fixed git

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,7 +1,7 @@
-Rails.configuration.stripe = {
-  :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
-  :secret_key      => ENV['STRIPE_SECRET_KEY']
-}
+# Rails.configuration.stripe = {
+#   :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
+#   :secret_key      => ENV['STRIPE_SECRET_KEY']
+# }
 
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
+# Stripe.api_key = Rails.configuration.stripe[:secret_key]
 
